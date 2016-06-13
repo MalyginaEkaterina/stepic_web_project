21af7332a8849000523e931f24037b55adca4dc6
MalyginaEkaterina <katty2007v@yandex.ru>
2016-06-13 23:12:38 +0300

    Added nginx conf

--- init.sh
+++ init.sh
@@ -1,10 +1,3 @@
 sudo apt-get install nginx
-mkdir home/box/web
-mkdir home/box/web/public
-mkdir home/box/web/public/img
-mkdir home/box/web/public/css
-mkdir home/box/web/public/js
-mkdir home/box/web/uploads
-mkdir home/box/web/etc
-sudo ln -s /etc/nginx/nginx.conf /home/box/web/etc/nginx.conf
-sudo nginx -s reload
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
