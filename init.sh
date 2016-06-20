55d087bd5b541d3260c8a9a95d2d848a88013f28
MalyginaEkaterina <katty2007v@yandex.ru>
2016-06-20 23:40:55 +0300

    gunicorn config and hello app.

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 sudo apt-get install nginx
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo apt-get install gunicorn
+sudo ln -s /home/box/web/etc/hello.conf /etc/gunicorn.d/hello.conf
+sudo /etc/init.d/gunicorn restart
